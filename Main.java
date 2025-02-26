058f62d8c819aa468d6ef6d1b97425434a5b7f00
Gabriel-Ricci <gabrielr244@nycstudents.net>
2025-02-26 14:25:44 +0000

    b

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void Main(String[] args){
+        Candy[][] box = new Candy[4][3];
+        box[0][1] = new Candy("lime");
+        box[1][1] = new Candy("orange");
+        box[2][2] = new Candy("cherry");
+        box[3][1] = new Candy("lemon");
+        box[3][2] = new Candy("grape");
+        
+
+    }
+}
